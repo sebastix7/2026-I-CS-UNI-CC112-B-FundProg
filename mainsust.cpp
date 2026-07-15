8ae556fa4a52c558cb58cc1b574c7f3eba6fbdfe
Sebastian <sebastian.illanes.g@uni.pe>
2026-07-15 15:43:21 -0500

    Solucion Examen Sustitutorio - Sebastian Illanes

--- mainsust.cpp
+++ mainsust.cpp
@@ -8,14 +8,14 @@ using namespace std;
 
 int main() {
     Ubicacion fc = {"Facultad de Ciencias - Aula R-201", -12.0163, -77.0498};
-    Ubicacion puerta3 = {"Puerta de Ingreso Nro 5", -12.0185, -77.0491};
+    Ubicacion puerta5 = {"Puerta de Ingreso Nro 5", -12.0185, -77.0491};
     Ubicacion campusCentral = {"Facultad de Ciencias UNI", -12.0172, -77.0505};
 
     TI totalComunidad = 3;
     Persona** comunidadUNI = new Persona*[totalComunidad];
 
     comunidadUNI[0] = new Profesor("Ernesto Cuadros", 19951042, fc, "Fundamentos de la Programacion");
-    comunidadUNI[1] = new Seguridad("Don Juan", 994012, puerta3, "Noche");
+    comunidadUNI[1] = new Seguridad("Don Juan", 994012, puerta5, "Noche");
     comunidadUNI[2] = new SupervisorSeguridad("Comandante Flores", 991204, campusCentral, "Mañana", "Motocicleta UNI-05");
 
     cout << "=== MONITOREO EN TIEMPO REAL - Facultad de Ciencias UNI ===\n\n";
